02cbe12f28876dcf776d21ac0ba320501cf37eb3
Simon Whale <Simon_Whale@allfleetservices.co.uk>
2019-01-19 20:48:50 +0000

    Add project files.

---
+++ ExceptionFilter/ExceptionFilter/ExceptionFilteringAttribute.cs
@@ -0,0 +1,48 @@
+﻿using ExceptionFilter.UserDefinedExceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ExceptionFilter.ExceptionFilter
+{
+    public class ExceptionHandling
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionHandling(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch(Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        private static Task HandleException(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError;
+
+            if (ex is UnauthorizedAccessException) code = HttpStatusCode.Unauthorized;
+            if (ex is NotRecordedIAException) code = HttpStatusCode.Forbidden;
+
+            var result = JsonConvert.SerializeObject(new { error = ex.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ ExceptionFilter/UserDefinedExceptions/NotRecordedIAException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionFilter.UserDefinedExceptions
+{
+    public class NotRecordedIAException : Exception
+    {
+        public NotRecordedIAException(string value) : base(value) { }
+    }
+}
